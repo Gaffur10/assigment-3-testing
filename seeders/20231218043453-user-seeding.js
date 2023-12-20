2e7cd341a9325e5f7f2c06eaa45b889a808bc82c
Gaffur10 <adegaffur@gmail.com>
2023-12-21 00:02:04 +0800

    commit

---
+++ seeders/20231218043453-user-seeding.js
@@ -0,0 +1,50 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          username: "gaffur12",
+          email: "gaffur12@gmail.com",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          username: "kevin13",
+          email: "kevin13@gmail.com",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          username: "lala14",
+          email: "lala14@gmail.com",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
---
+++ seeders/20231218044226-photo-seeding-with-user.js
@@ -0,0 +1,71 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "Photos",
+      [
+        {
+          title: "Photo 1",
+          caption: "Caption photo 1",
+          image_url: "https://picsum.photos/id/1/500/300",
+          UserId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: "Photo 2",
+          caption: "Caption photo 2",
+          image_url: "https://picsum.photos/id/2/500/300",
+          UserId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: "Photo 3",
+          caption: "Caption photo 3",
+          image_url: "https://picsum.photos/id/3/500/300",
+          UserId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: "Photo 4",
+          caption: "Caption photo 4",
+          image_url: "https://picsum.photos/id/4/500/300",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          UserId: 2,
+        },
+        {
+          title: "Photo 5",
+          caption: "Caption photo 5",
+          image_url: "https://picsum.photos/id/5/500/300",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          UserId: 3,
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
